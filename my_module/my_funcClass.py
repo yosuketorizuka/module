b83443fbd4fea2ab4ce9b8614f95b8636cb56417
yosuketorizuka <yosuketorizuka@gmail.com>
2021-02-27 14:21:00 +0900

    csv読み込みをモジュール化

--- my_module/my_funcClass.py
+++ my_module/my_funcClass.py
@@ -1,13 +1,18 @@
 class my_funcClass:
 
-    def __init__(self, x,y):
+#    def __init__(self, x,y):
+#        self.x = x
+#        self.y = y
+
+    def my_sum(self, x, y):
         self.x = x
         self.y = y
 
-    def my_sum(self, x, y):
         z = self.x + self.y
         return z
 
     def my_dif(self, x,y):
+        self.x = x
+        self.y = y
         z = x - y
         return z
--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from my_module import my_funcClass
 
 if __name__ == '__main__':
     
-    ins_1 = my_funcClass.my_funcClass(2,1)
-    ins_2 = my_funcClass.my_funcClass(2,1)
+    ins_1 = my_funcClass.my_funcClass()
+    ins_2 = my_funcClass.my_funcClass()
     
     out1 = ins_1.my_sum(2,1)
     out2 = ins_2.my_dif(2,1)
