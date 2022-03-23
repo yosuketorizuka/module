c2c0eafc695ad1d802ce7146cb6340a70cf1184f
Yosuke Torizuka <bird.since.1983@gmail.com>
2022-03-24 07:11:18 +0900

    20220324

--- test.py
+++ test.py
@@ -1,17 +1,35 @@
-from myModules import zzModules
+# %%
 
-importFilePath = r'DiffTool/import/'
-importFileName1 = r'Trends_in_deaths1.csv'
+from pydoc import importfile
+import pandas as pd
+import csv
+from myModules import  zzModules
+import myModules
 
+importFilePath = r'import/'
+importFileName = r'import.csv'
+exportFilePath = r'export/'
+exportFileName = r'export.csv'
+
+def readFile():
+    
+    global df_import_in
+    
+    read_import = zzModules.readFileClass()
+    read_import.setPara(importFilePath, importFileName)
+    df_import_in = read_import.readFile()
+
+def writeFile():
+    
+    write_export = zzModules.writeFileClass()
+    write_export.setPara(exportFilePath, exportFileName, df_import_in)
+    write_export.writeFile()
 
 if __name__ == "__main__":
     
-    readFile = zzModules.readFileClass()
-    readFile.setPara(importFilePath, importFileName1)
-    dfFile = readFile.readFile()
+    readFile()
 
-    getCol = zzModules.getColClass()
-    getCol.setPara(dfFile)
-    listCol = getCol.getCol()
-
-    pass
+    writeFile()
+    
+    print("process completed")
+# %%
